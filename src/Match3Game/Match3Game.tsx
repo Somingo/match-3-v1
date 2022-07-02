2470a5919f9fe590a08cddea6517bc9e4dcaf8f5
Soma Sörös <soma.soros@dpc.hu>
2022-07-02 20:46:16 +0200

    add gravity for gems

--- src/Match3Game/Match3Game.tsx
+++ src/Match3Game/Match3Game.tsx
@@ -25,33 +25,68 @@ const initialState: IGameState = {
     ], cursor: 35,
 };
 
-const gameStateReducer = (s: IGameState, a: number): IGameState => {
-    switch (a) {
-        // left 37
-        case 37:
-            return {...s, cursor: s.cursor % LINE_WIDTH - 1 > -1 ? s.cursor - 1 : s.cursor};
-        // up 38HEIGHT
-        case 38:
-            return {...s, cursor: s.cursor - LINE_WIDTH > -1 ? s.cursor - LINE_WIDTH : s.cursor};
-        // right 39
-        case 39:
-            return {...s, cursor: s.cursor % LINE_WIDTH + 2 < LINE_WIDTH ? s.cursor + 1 : s.cursor};
-        // down 40
-        case 40:
-            return {
-                ...s,
-                cursor: s.cursor + LINE_WIDTH < s.table.length - 1 ? s.cursor + LINE_WIDTH : s.cursor,
-            };
-        // space 32
-        case 32:
-            const nextState = [...s.table];
-            const a = nextState[s.cursor];
-            nextState[s.cursor] = nextState[s.cursor + 1];
-            nextState[s.cursor + 1] = a;
-            return {...s, table: nextState};
-
+const moveCursorLeft = (state: IGameState): IGameState => ({
+    ...state,
+    cursor: state.cursor % LINE_WIDTH - 1 > -1 ? state.cursor - 1 : state.cursor,
+});
+const moveCursorUp = (state: IGameState): IGameState => ({
+    ...state,
+    cursor: state.cursor - LINE_WIDTH > -1 ? state.cursor - LINE_WIDTH : state.cursor,
+});
+const moveCursorRight = (state: IGameState): IGameState => ({
+    ...state,
+    cursor: state.cursor % LINE_WIDTH + 2 < LINE_WIDTH ? state.cursor + 1 : state.cursor,
+});
+const moveCursorDown = (state: IGameState): IGameState => ({
+    ...state,
+    cursor: state.cursor + LINE_WIDTH < state.table.length - 1 ? state.cursor + LINE_WIDTH : state.cursor,
+});
+const swapGems = (state: IGameState): IGameState => {
+    const nextTable = [...state.table];
+    const a = nextTable[state.cursor];
+    nextTable[state.cursor] = nextTable[state.cursor + 1];
+    nextTable[state.cursor + 1] = a;
+    return {...state, table: nextTable};
+};
+const applyGravityIteration = (nextTable: number[]):boolean => {
+    let hasMoving = false;
+    for (let i = nextTable.length-1-LINE_WIDTH; i > -1; i--) {
+        if (nextTable[i+LINE_WIDTH] === 0 && nextTable[i] !== 0) {
+            nextTable[i+LINE_WIDTH] = nextTable[i];
+            nextTable[i] = 0;
+            hasMoving = true;
+        }
     }
-    return s;
+    return hasMoving;
+}
+const applyGravity = (state: IGameState): IGameState => {
+    const nextTable = [...state.table];
+    while(applyGravityIteration(nextTable)) {}
+    return {...state, table: nextTable};
+}
+
+const gameStateReducer = (state: IGameState, action: number): IGameState => {
+    switch (action) {
+        // move cursor left 37
+        case 37:
+            return moveCursorLeft(state);
+        // move cursor up 38
+        case 38:
+            return moveCursorUp(state);
+        // move cursor right 39
+        case 39:
+            return moveCursorRight(state);
+        // move cursor down 40
+        case 40:
+            return moveCursorDown(state);
+        // swap gems space 32
+        case 32:
+            return applyGravity(swapGems(state));
+        // gravity 1 iteration G button
+        case 71:
+            return applyGravity(state);
+    }
+    return state;
 };
 
 export const Match3Game: FC = () => {
@@ -60,6 +95,7 @@ export const Match3Game: FC = () => {
     useEffect(() => {
         const keyPressListener = (e: KeyboardEvent) => {
             dispatch(e.keyCode);
+            console.log(e.keyCode);
         };
         document.body.addEventListener('keyup', keyPressListener);
         return () => {
