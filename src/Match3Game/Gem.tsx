78e2c6fb783b9aa454022ca93d2f71b4fda7f3b5
Soma Sörös <soma.soros@dpc.hu>
2022-07-02 23:06:09 +0200

    remove gems

--- src/Match3Game/Gem.tsx
+++ src/Match3Game/Gem.tsx
@@ -1,19 +1,31 @@
 import React, {FC} from 'react';
 import {Box, SxProps, Theme} from '@mui/material';
 
+export interface IGem {
+    type: number;
+    points: number;
+}
+
 const cursorLeftSx: SxProps<Theme> = {
-  borderColor: '#000 #fff #000 #000',
+    borderColor: '#000 #fff #000 #000',
 };
 
 const cursorRightSx: SxProps<Theme> = {
-  borderColor: '#000 #000 #000 #fff',
+    borderColor: '#000 #000 #000 #fff',
 };
 
 export const gemColors = ['#fff', '#f00', '#0f0', '#00f', '#f0f']
 
-export const Gem: FC<{type: number, cursorLeft: boolean, cursorRight: boolean}> = ({type, cursorLeft, cursorRight}) => {
-    return <Box sx={{flex: '14% 0 0', paddingTop: '14%', backgroundColor: gemColors[type], border: '1px solid #fff',
-      ...(cursorLeft?cursorLeftSx:{}),
-      ...(cursorRight?cursorRightSx:{}),
+export interface GemProps {
+    gem: IGem,
+    cursorLeft: boolean,
+    cursorRight: boolean
+}
+
+export const Gem: FC<GemProps> = ({gem: {type, points}, cursorLeft, cursorRight}) => {
+    return <Box sx={{
+        flex: '14% 0 0', paddingTop: '14%', backgroundColor: gemColors[type], border: '1px solid #fff',
+        ...(cursorLeft ? cursorLeftSx : {}),
+        ...(cursorRight ? cursorRightSx : {}),
     }}/>
 }
--- src/Match3Game/Match3Game.tsx
+++ src/Match3Game/Match3Game.tsx
@@ -1,18 +1,23 @@
 import React, {FC, useEffect, useReducer} from 'react';
 import {Box, Typography} from '@mui/material';
-import {Gem, gemColors} from './Gem';
+import {Gem, gemColors, IGem} from './Gem';
 
 const LINE_WIDTH = 7;
 const LINES = 11;
 
 interface IGameState {
     cursor: number;
-    table: number[];
+    table: IGem[];
+    score: number;
 }
 
 const initialState: IGameState = {
-    table: new Array(LINE_WIDTH * LINES).fill(0).map(() => Math.floor(Math.random() * gemColors.length)),
+    table: new Array(LINE_WIDTH * LINES)
+        .fill(0)
+        .map(() => Math.floor(Math.random() * gemColors.length))
+        .map<IGem>(type => ({type, points: 0})),
     cursor: 35,
+    score: 0,
 };
 
 const moveCursorLeft = (state: IGameState): IGameState => ({
@@ -38,12 +43,13 @@ const swapGems = (state: IGameState): IGameState => {
     nextTable[state.cursor + 1] = a;
     return {...state, table: nextTable};
 };
-const applyGravityIteration = (nextTable: number[]): boolean => {
+const applyGravityIteration = (nextTable: IGem[]): boolean => {
     let hasMoving = false;
     for (let i = nextTable.length - 1 - LINE_WIDTH; i > -1; i--) {
-        if (nextTable[i + LINE_WIDTH] === 0 && nextTable[i] !== 0) {
+        if (nextTable[i + LINE_WIDTH].type === 0 && nextTable[i].type !== 0) {
+            const temp = nextTable[i + LINE_WIDTH];
             nextTable[i + LINE_WIDTH] = nextTable[i];
-            nextTable[i] = 0;
+            nextTable[i] = temp;
             hasMoving = true;
         }
     }
@@ -54,6 +60,40 @@ const applyGravity = (state: IGameState): IGameState => {
     while (applyGravityIteration(nextTable)) {
     }
     return {...state, table: nextTable};
+}
+const markForRemove = (nextTable: IGem[]): boolean => {
+    let hasRemovable = false;
+    nextTable.forEach(gem => {
+        gem.points = 0
+    });
+    // horizontal
+    for (let i = 0; i < nextTable.length; i++) {
+        if (nextTable[i].type !== 0 && i % LINE_WIDTH < LINE_WIDTH - 2 && nextTable[i].type === nextTable[i + 1].type && nextTable[i].type === nextTable[i + 2].type) {
+            nextTable[i].points++;
+            nextTable[i + 1].points++;
+            nextTable[i + 2].points++;
+            hasRemovable = true;
+        }
+    }
+    // vertical
+    for (let i = 0; i < nextTable.length; i++) {
+        if (nextTable[i].type !== 0 && i / LINE_WIDTH < LINES - 2 && nextTable[i].type === nextTable[i + LINE_WIDTH].type && nextTable[i].type === nextTable[i + LINE_WIDTH * 2].type) {
+            nextTable[i].points++;
+            nextTable[i + LINE_WIDTH].points++;
+            nextTable[i + LINE_WIDTH * 2].points++;
+            hasRemovable = true;
+        }
+    }
+    return hasRemovable;
+}
+
+const removeGems = (state: IGameState): IGameState => {
+    if (markForRemove(state.table)) {
+        const additionalScore = state.table.reduce((points, gem) => points + gem.points, 0);
+        const nextTable = state.table.map(gem => gem.points ? {type: 0, points: 0} : gem);
+        return removeGems(applyGravity({...state, table: nextTable, score: state.score + additionalScore}));
+    }
+    return state;
 }
 
 const gameStateReducer = (state: IGameState, action: number): IGameState => {
@@ -72,16 +112,19 @@ const gameStateReducer = (state: IGameState, action: number): IGameState => {
             return moveCursorDown(state);
         // swap gems space 32
         case 32:
-            return applyGravity(swapGems(state));
+            return removeGems(applyGravity(swapGems(state)));
         // gravity 1 iteration G button
         case 71:
             return applyGravity(state);
+        // mark for remove gems M Button
+        case 77:
+            return removeGems(state);
     }
     return state;
 };
 
 export const Match3Game: FC = () => {
-    const [{table, cursor}, dispatch] = useReducer(gameStateReducer, initialState, applyGravity);
+    const [{table, cursor, score}, dispatch] = useReducer(gameStateReducer, initialState, removeGems);
 
     useEffect(() => {
         const keyPressListener = (e: KeyboardEvent) => {
@@ -102,10 +145,10 @@ export const Match3Game: FC = () => {
             justifyContent: 'center',
             alignItems: 'center',
         }}>
-            <Typography variant="h3">Game</Typography>
+            <Typography variant="h3">Score: {score}</Typography>
             <Box sx={{display: 'flex', width: '100%', maxWidth: '500px', flexWrap: 'wrap'}}>
-                {table.map((n, index) => <Gem type={n} cursorLeft={cursor === index}
-                                              cursorRight={cursor + 1 === index}/>)}
+                {table.map((gem, index) => <Gem gem={gem} cursorLeft={cursor === index}
+                                                cursorRight={cursor + 1 === index}/>)}
             </Box>
         </Box>
     );
